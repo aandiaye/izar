2c52f57a2a312be4886c6b4bddd0054c2d1cc0b4
ABDOUL AZIZ NDIAYE <a.abdoul.ndiaye@gmail.com>
2022-10-17 19:35:25 +0000

    ajout de la root identification

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def login():
 @app.route('/index')
 def home():
     return render_template('index.html')
+@app.route('/identification')
+def identification():
+    return render_template('identification.html')
+
 
 if __name__ == '__main__':
     app.run(port=5005, debug=True)
